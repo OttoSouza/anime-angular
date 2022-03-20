0e4fa6e92f2acfa324d2408713765c9a80134d26
OttoSouza <ottos.developer@gmail.com>
2022-03-20 11:15:10 -0300

    create list-anime

---
+++ src/app/components/list-animes/list-animes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-animes',
+  templateUrl: './list-animes.component.html',
+  styleUrls: ['./list-animes.component.css']
+})
+export class ListAnimesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
